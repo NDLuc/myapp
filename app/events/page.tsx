fae6f0da671366070083a91f6710b50a85bfe7dc
v0agent <it+v0agent@vercel.com>
2026-06-14 09:12:15 +0000

    Restored v5: Reset to default branch after deleted branch

--- app/events/page.tsx
+++
@@ -1,7 +0,0 @@
-import { EventsScreen } from "@/components/events-screen"
-import { getEvents } from "@/lib/events"
-
-export default async function EventsPage() {
-  const events = await getEvents()
-  return <EventsScreen events={events} />
-}
